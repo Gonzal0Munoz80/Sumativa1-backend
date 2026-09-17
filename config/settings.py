058994f42729516699860d4a247f7593cc566750
Gonzal0Munoz80 <Gonzal0Munoz80@users.noreply.github.com>
2026-09-17 10:51:33 -0300

    Commit 2 estructura con informacion de juegos y html inciiales

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inicio'
-    'detalle'
+    'inicio',
+    'detalle',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-static_dir = 'static'
+STATICFILES_DIRS = [Static_dir]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
--- detalle/views.py
+++ detalle/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def detalle(request):
+    juego = {
+        'id': 1,
+        'titulo': 'Caminos del Norte',
+        'plataforma': 'PC',
+        'precio': 24990,
+        'horas': 30,
+        'multijugador': False,
+        'oferta': False,
+        'descripcion': 'Aventura de mundo abierto por el desierto de Atacama, con misiones en pueblos mineros y caletas.',
+    }
+    return render(request, 'detalle.html', {'juego': juego})
+
